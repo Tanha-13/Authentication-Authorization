c179e18f3546d821f8c315b934db051edbb6005b
Tanha <nusrattanha897@gmail.com>
2024-11-17 01:07:02 +0600

    correction about database

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,6 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.listen(3000,() => {
-    console.log("Server is running on 3000");
-})
+//
+const dotenv = require("dotenv");
+dotenv.config();
+
+const mongoose = require("mongoose");
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Database is connected");
+  })
+  .catch((error) => {
+    console.log(`The error is ${error}`);
+  });
+
+  
+
+app.listen(3000, () => {
+  console.log("Server is running on 3000");
+});
