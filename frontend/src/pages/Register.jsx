dd208b313417c47456833bb2a66fc560c4408b53
Tanha <nusrattanha897@gmail.com>
2024-11-20 02:11:40 +0600

    complete signup functionality

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,21 +1,78 @@
-import {Link} from "react-router-dom"
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 function Register() {
+  const [formData, setFormData] = useState({});
+  const [error,setError] = useState(false);
+  const [loading,setLoading] = useState(false);
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try{
+      setLoading(true);
+      const response = await fetch('http://localhost:5000/api/auth/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type' : 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await response.json();
+      console.log(data);
+      setLoading(false);
+      if(data.success === false){
+        setError(true);
+        console.log(error);
+        return;
+      }
+    }
+    catch(err){
+      setLoading(false);
+      setError(true);
+      console.log(error);
+    }
+  }
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Register</h1>
-      <form action="" className="flex flex-col  gap-4">
-        <input type="text"  placeholder="Username" id="username" className="bg-slate-100 p-3 rounded-lg"/>
-        <input type="email"  placeholder="Email" id="Email" className="bg-slate-100 p-3 rounded-lg"/>
-        <input type="password"  placeholder="Password" id="password" className="bg-slate-100 p-3 rounded-lg"/>
-        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80 font-medium text-xl">Register</button>
+      <form onSubmit={handleSubmit} action="" className="flex flex-col  gap-4">
+        <input
+          type="text"
+          placeholder="Username"
+          id="username"
+          className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          id="email"
+          className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          id="password"
+          className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        <button disabled={loading} className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80 font-medium text-xl">
+          {loading ? "Loading..." : 'Register'}
+        </button>
       </form>
       <div className="flex gap-2 mt-3">
         <p>Have an account?</p>
-        <Link className="text-blue-500 font-medium" to ="/login">Login</Link>
+        <Link className="text-blue-500 font-medium" to="/login">
+          Login
+        </Link>
       </div>
+      <p className="text-red-700">{error ?? "Something went wrong"}</p>
     </div>
-  )
+  );
 }
 
-export default Register
+export default Register;
