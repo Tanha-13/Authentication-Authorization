898885d870e80f984b0fb77e1b0d3ee59a507ca0
Tanha <nusrattanha897@gmail.com>
2024-11-24 00:16:16 +0600

    create signin api routes

--- backend/api/index.js
+++ backend/api/index.js
@@ -2,8 +2,10 @@
 
 const express = require("express");
 const app = express();
+const cors = require("cors");
 // global middleware for parsing JSON requests
 app.use(express.json());
+app.use(cors());
 
 // environment variable setup
 const dotenv = require("dotenv");
@@ -21,13 +23,18 @@ mongoose
     console.log(`The error is ${error}`);
   });
 
+  
+  app.listen(5000, () => {
+    console.log("Server is running on 5000");
+  });
+
   // user routes
-  const userRoutes = require('./routes/user.route');
+  const userRoutes = require('./routes/user.route.js');
   app.use("/api/user", userRoutes);
 
   // creating auth routes
-  const authRoutes = require('./routes/auth.route');
-  app.use('/api/auth', authRoutes);
+  const authRoutes = require('./routes/auth.route.js');
+  app.use("/api/auth", authRoutes);
 
 
 // error middleware
@@ -42,7 +49,3 @@ app.use((err,req,res,next) => {
 })
 
 
-
-  app.listen(3000, () => {
-    console.log("Server is running on 3000");
-  });
